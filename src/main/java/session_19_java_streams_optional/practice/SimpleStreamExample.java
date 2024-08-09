82805b8a165c3ca45673ed555695e95786c9a569
raduM <23radumihaiu@gmail.com>
2024-08-09 19:44:38 +0300

    Session 19 - add

---
+++ src/main/java/session_19_java_streams_optional/practice/SimpleStreamExample.java
@@ -0,0 +1,14 @@
+package session_19_java_streams_optional.practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SimpleStreamExample {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("John", "Jack", "Alice");
+        names.stream().forEach( n -> System.out.println(n) );
+
+    }
+}
